48c7a046d5e112c65417c84466219f81c1dadfd2
brill3000 <bkaboyee@gmail.com>
2022-08-05 21:48:11 +0300

    Add Redux Logger for logging in development

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 // project import
 import reducers from './reducers';
+import logger from 'redux-logger';
 
 // ==============================|| REDUX TOOLKIT - MAIN STORE ||============================== //
 
 const store = configureStore({
-    reducer: reducers
+    reducer: reducers,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 });
 
 const { dispatch } = store;
