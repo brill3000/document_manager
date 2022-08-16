d761f5998064e97c25d372fd9eb88503245b3600
brill3000 <bkaboyee@gmail.com>
2022-08-16 14:56:40 +0300

    Edit departments children navigation

--- src/menu-items/dashboard.js
+++ src/menu-items/dashboard.js
@@ -5,6 +5,8 @@ import { HddOutlined } from '@ant-design/icons';
 import { FolderOutlined } from '@ant-design/icons';
 import { BarsOutlined } from '@ant-design/icons';
 import { TagsOutlined } from '@ant-design/icons';
+import { RiseOutlined } from '@ant-design/icons';
+import { DatabaseOutlined } from '@ant-design/icons';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import NoteAddIcon from '@mui/icons-material/NoteAdd';
 import PostAddOutlinedIcon from '@mui/icons-material/PostAddOutlined';
@@ -13,7 +15,15 @@ import FolderDeleteOutlinedIcon from '@mui/icons-material/FolderDeleteOutlined';
 import DifferenceOutlinedIcon from '@mui/icons-material/DifferenceOutlined';
 import FiberNewOutlinedIcon from '@mui/icons-material/FiberNewOutlined';
 import FileOpenOutlinedIcon from '@mui/icons-material/FileOpenOutlined';
-// icons
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
+import DesktopWindowsOutlinedIcon from '@mui/icons-material/DesktopWindowsOutlined';
+import NoteAltOutlinedIcon from '@mui/icons-material/NoteAltOutlined';
+import GavelOutlinedIcon from '@mui/icons-material/GavelOutlined';
+import SettingsPhoneOutlinedIcon from '@mui/icons-material/SettingsPhoneOutlined';
+import BiotechOutlinedIcon from '@mui/icons-material/BiotechOutlined';
+import ConnectWithoutContactOutlinedIcon from '@mui/icons-material/ConnectWithoutContactOutlined';
+import MiscellaneousServicesOutlinedIcon from '@mui/icons-material/MiscellaneousServicesOutlined';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';// icons
 const icons = {
     DashboardOutlined,
     ApartmentOutlined,
@@ -29,6 +39,17 @@ const icons = {
     DifferenceOutlinedIcon,
     FiberNewOutlinedIcon,
     FileOpenOutlinedIcon,
+    RiseOutlined,
+    DatabaseOutlined,
+    MonetizationOnOutlinedIcon,
+    DesktopWindowsOutlinedIcon,
+    NoteAltOutlinedIcon,
+    GavelOutlinedIcon,
+    SettingsPhoneOutlinedIcon,
+    BiotechOutlinedIcon,
+    ConnectWithoutContactOutlinedIcon,
+    MiscellaneousServicesOutlinedIcon,
+    LocalShippingOutlinedIcon
 };
 
 // ==============================|| MENU ITEMS - DASHBOARD ||============================== //
@@ -118,7 +139,81 @@ const dashboard = {
             type: 'item',
             url: '/departments',
             icon: icons.ApartmentOutlined,
-            breadcrumbs: false
+            breadcrumbs: false,
+            children: [
+                {
+                    id: 'clerksoffice',
+                    title: 'Clerks Office',
+                    type: 'item',
+                    url: '/departments/clerksoffice',
+                    icon: icons.MiscellaneousServicesOutlinedIcon,
+                    breadcrumbs: false
+                },
+                {
+                    id: 'finance',
+                    title: 'Finance',
+                    type: 'item',
+                    url: '/departments/finance',
+                    icon: icons.MonetizationOnOutlinedIcon,
+                    breadcrumbs: false
+                },
+                {
+                    id: 'supplychain',
+                    title: 'Supply Chain',
+                    type: 'item',
+                    url: '/departments/supplychain',
+                    icon: icons.LocalShippingOutlinedIcon,
+                    breadcrumbs: false
+                },
+                {
+                    id: 'research_and_records',
+                    title: 'Research And Records',
+                    type: 'item',
+                    url: '/departments/research_and_records',
+                    icon: icons.BiotechOutlinedIcon,
+                    breadcrumbs: false
+                },
+                {
+                    id: 'hansard',
+                    title: 'Hansard',
+                    type: 'item',
+                    url: '/departments/hansard',
+                    icon: icons.NoteAltOutlinedIcon,
+                    breadcrumbs: false
+                },
+                {
+                    id: 'ict',
+                    title: 'ICT',
+                    type: 'item',
+                    url: '/departments/ict',
+                    icon: icons.DesktopWindowsOutlinedIcon,
+                    breadcrumbs: false
+                },
+                {
+                    id: 'clerks_at_the_table',
+                    title: 'Clerks at The Table',
+                    type: 'item',
+                    url: '/departments/clerks_at_the_table',
+                    icon: icons.MiscellaneousServicesOutlinedIcon,
+                    breadcrumbs: false
+                },
+                {
+                    id: 'sergent_at_arms',
+                    title: 'Sergent at arms',
+                    type: 'item',
+                    url: '/departments/sergent_at_arms',
+                    icon: icons.GavelOutlinedIcon,
+                    breadcrumbs: false
+                },
+                {
+                    id: 'reception',
+                    title: 'Reception',
+                    type: 'item',
+                    url: '/departments/reception',
+                    icon: icons.SettingsPhoneOutlinedIcon,
+                    breadcrumbs: false
+                }
+            ]
         },
         // {
         //     id: 'cabinets',
@@ -133,15 +228,15 @@ const dashboard = {
             title: 'Approvals',
             type: 'item',
             url: '/approvals',
-            icon: icons.BarsOutlined,
+            icon: icons.RiseOutlined,
             breadcrumbs: false
         },
         {
-            id: 'tags',
-            title: 'Tags',
+            id: 'indexes',
+            title: 'Indexes',
             type: 'item',
-            url: '/',
-            icon: icons.TagsOutlined,
+            url: '/indexes',
+            icon: icons.DatabaseOutlined,
             breadcrumbs: false
         }
     ]
