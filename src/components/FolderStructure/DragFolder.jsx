594754f47ac80108e4b3beff5932c3e388c4c016
brill3000 <bkaboyee@gmail.com>
2022-08-16 14:56:17 +0300

    Add drag drop functionality to folder viewer

---
+++ src/components/FolderStructure/DragFolder.jsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { useDrag } from 'react-dnd';
+
+
+
+
+
+export const DragFolder = React.forwardRef(({ children, ...props }, ref) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    // "type" is required. It is used by the "accept" specification of drop targets.
+    type: 'Folder',
+    // The collect function utilizes a "monitor" instance (see the Overview for what this is)
+    // to pull important pieces of state from the DnD system.
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging()
+    })
+  }));
+  return (
+    <div ref={drag} {...props}>
+      {children}
+    </div>
+  );
+});
+DragFolder.propTypes = {
+  children: PropTypes.node.isRequired,
+};
