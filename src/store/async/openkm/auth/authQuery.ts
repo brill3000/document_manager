35b9000c3e8bc39d55504b024856327f09b99c9e
brill3000 <bkaboyee@gmail.com>
2023-05-18 09:39:58 +0300

    add auth query

--- src/store/async/openkm/auth/authQuery.ts
+++ src/store/async/openkm/auth/authQuery.ts
@@ -1,3 +1,31 @@
-export function name(params: { title: 'test' }) {
-    return params.title;
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+interface Post {
+    id: number;
+    name: string;
 }
+
+const api = createApi({
+    reducerPath: 'auth_query',
+    baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+    endpoints: (build) => ({
+        getPost: build.query<Post, number>({
+            queryFn: (arg, queryApi, extraOptions, baseQuery) => {
+                if (arg <= 0) {
+                    return {
+                        error: {
+                            status: 500,
+                            statusText: 'Internal Server Error',
+                            data: 'Invalid ID provided.'
+                        }
+                    };
+                }
+                const post: Post = {
+                    id: arg,
+                    name: 'Brilliant'
+                };
+                return { data: post };
+            }
+        })
+    })
+});
