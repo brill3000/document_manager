5158028c1d6312a442ba98e57d700b5a8e948d3d
brill3000 <bkaboyee@gmail.com>
2023-05-22 13:45:21 +0300

    code clean up

--- src/components/documents/index.tsx
+++ src/components/documents/index.tsx
@@ -2,16 +2,9 @@ import React from 'react';
 import FileBrowser from './components/FileBrowser';
 import '@fontsource/quicksand';
 import './Themes/Sass/App.css';
-import { useGetUsersQuery } from 'store/async/dms/auth/authApi';
+
 const FolderView = ({ title }: { title: string }) => {
-    const { data, error, isLoading } = useGetUsersQuery({});
-    React.useEffect(() => {
-        if (error) {
-            console.log(error, 'ERROR');
-        }
-        console.log(data, 'DATA');
-    }, [isLoading, error]);
-    return <FileBrowser title={title} height="80vh" width="100%" />;
+    return <FileBrowser title={title} height="85vh" width="100%" />;
 };
 
 export default FolderView;
