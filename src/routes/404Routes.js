8de54c9436bfd71409cd996c96753f8934097c4b
brill3000 <bkaboyee@gmail.com>
2023-06-07 10:12:11 +0300

    Add 404 page

---
+++ src/routes/404Routes.js
@@ -0,0 +1,11 @@
+// project import
+import NotFound from 'layout/404Layout/NotFound';
+
+// ==============================|| AUTH ROUTING ||============================== //
+
+const NotFoundRoute = {
+    path: '*',
+    element: <NotFound />
+};
+
+export default NotFoundRoute;
