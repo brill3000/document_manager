b4fe94906945e3465d60a147d356fb7aa9eb5aeb
brill3000 <bkaboyee@gmail.com>
2023-06-14 16:49:34 +0300

    center icons

--- src/assets/images/icons/FolderIcon.tsx
+++ src/assets/images/icons/FolderIcon.tsx
@@ -6,6 +6,8 @@ function FolderIcon({ size, selected }: { size: number; selected?: boolean }) {
     return (
         <Box
             width={size ? (selected ? size + resize : size) : 30}
+            display="flex"
+            alignItems="center"
             sx={{
                 transition: '0.2s all',
                 transitionTimingFunction: 'cubic-bezier(0.25,0.1,0.25,1)'
@@ -15,13 +17,13 @@ function FolderIcon({ size, selected }: { size: number; selected?: boolean }) {
                 <path
                     d="M75,8.33H37.5L29.17,0H8.34A8.36,8.36,0,0,0,0,8.33V25H83.34V16.66A8.36,8.36,0,0,0,75,8.33Z"
                     transform="translate(0 0)"
-                    fill={selected ? '#e18f02' : '#ffa000'}
+                    fill={selected ? '#f89e02' : '#ffa000'}
                     style={{ transition: '0.1s all', transitionTimingFunction: 'cubic-bezier(0.25,0.1,0.25,1)' }}
                 />
                 <path
                     d="M75,8.33H8.34A8.36,8.36,0,0,0,0,16.66V58.33a8.36,8.36,0,0,0,8.34,8.33H75a8.36,8.36,0,0,0,8.34-8.33V16.66A8.36,8.36,0,0,0,75,8.33Z"
                     transform="translate(0 0)"
-                    fill={selected ? '#ddae1d' : '#ffca28'}
+                    fill={selected ? '#e4b831' : '#ffca28'}
                     style={{ transition: '0.1s all', transitionTimingFunction: 'cubic-bezier(0.25,0.1,0.25,1)' }}
                 />
             </svg>
--- src/assets/images/icons/FolderOpenIcon.tsx
+++ src/assets/images/icons/FolderOpenIcon.tsx
@@ -6,6 +6,8 @@ function FolderOpenIcon({ size, selected }: { size: number; selected?: boolean }
     return (
         <Box
             width={size ? (selected ? size + resize : size) : 30}
+            display="flex"
+            alignItems="center"
             sx={{
                 transition: '0.2s all',
                 transitionTimingFunction: 'cubic-bezier(0.25,0.1,0.25,1)'
@@ -15,12 +17,14 @@ function FolderOpenIcon({ size, selected }: { size: number; selected?: boolean }
                 <path
                     d="M34,4H18L14,0H4A4,4,0,0,0,0,4V28a4,4,0,0,0,4,4H35a3,3,0,0,0,3-3V8A4,4,0,0,0,34,4Z"
                     transform="translate(0 0)"
-                    fill={selected ? '#e18f02' : '#ffa000'}
+                    fill={selected ? '#f89e02' : '#ffa000'}
+                    style={{ transition: '0.1s all', transitionTimingFunction: 'cubic-bezier(0.25,0.1,0.25,1)' }}
                 />
                 <path
                     d="M38.2,10H11.3a4,4,0,0,0-3.9,3.3L4,32H35.7a4,4,0,0,0,3.9-3.3l2.5-14A4,4,0,0,0,38.2,10Z"
                     transform="translate(0 0)"
-                    fill={selected ? '#ddae1d' : '#ffca28'}
+                    fill={selected ? '#e4b831' : '#ffca28'}
+                    style={{ transition: '0.1s all', transitionTimingFunction: 'cubic-bezier(0.25,0.1,0.25,1)' }}
                 />
             </svg>
         </Box>
