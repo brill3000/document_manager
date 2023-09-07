f9dbc2d559f0e94170faf0a0111c8581a190a642
brill3000 <bkaboyee@gmail.com>
2023-09-07 23:12:13 +0300

    add error boundary

--- src/App.js
+++ src/App.js
@@ -8,30 +8,53 @@ import { FileUploaded } from 'ui-component/alerts/documents';
 import { UserAuthContextProvider } from 'context/authContext';
 import { TouchBackend } from 'react-dnd-touch-backend';
 import '@fontsource/montserrat';
+import { ErrorBoundary } from 'react-error-boundary';
 
 // ==============================|| APP - THEME, ROUTER, LOCAL  ||============================== //
-
+function MyFallbackComponent({ error, resetErrorBoundary }) {
+    return (
+        <div role="alert">
+            <p>Something went wrong:</p>
+            <pre>{error.message}</pre>
+            <button onClick={resetErrorBoundary}>Try again</button>
+        </div>
+    );
+}
+// Error logging function
+function logErrorToService(error, info) {
+    // Use your preferred error logging service
+    console.error('Caught an error:', error, info);
+}
 const App = () => {
     return (
-        <SnackbarProvider
-            anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right'
+        <ErrorBoundary
+            FallbackComponent={MyFallbackComponent}
+            onReset={() => {
+                // reset the state of your app here
             }}
-            Components={{
-                fileUploaded: FileUploaded
-            }}
+            onError={logErrorToService}
+            resetKeys={['someKey']}
         >
-            <UserAuthContextProvider>
-                <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
-                    <ThemeCustomization>
-                        <ScrollTop>
-                            <Routes />
-                        </ScrollTop>
-                    </ThemeCustomization>
-                </DndProvider>
-            </UserAuthContextProvider>
-        </SnackbarProvider>
+            <SnackbarProvider
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right'
+                }}
+                Components={{
+                    fileUploaded: FileUploaded
+                }}
+            >
+                <UserAuthContextProvider>
+                    <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
+                        <ThemeCustomization>
+                            <ScrollTop>
+                                <Routes />
+                            </ScrollTop>
+                        </ThemeCustomization>
+                    </DndProvider>
+                </UserAuthContextProvider>
+            </SnackbarProvider>
+        </ErrorBoundary>
     );
 };
 
